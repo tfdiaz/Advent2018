df172321fdace08095f11fa7be06942ac6a5672a
Tomas Diaz <tdiaz@student.42.us.org>
2019-02-01 15:07:02 -0800

    Saving files

---
+++ day16/target/debug/day16.d
@@ -0,0 +1,1 @@
+/private/tmp/Advent2018/day16/target/debug/day16.dSYM: /private/tmp/Advent2018/day16/src/main.rs
---
+++ day16/target/debug/deps/day16-a1d3a6cf5b1a3973.d
@@ -0,0 +1,5 @@
+/private/tmp/Advent2018/day16/target/debug/deps/day16-a1d3a6cf5b1a3973: src/main.rs
+
+/private/tmp/Advent2018/day16/target/debug/deps/day16-a1d3a6cf5b1a3973.d: src/main.rs
+
+src/main.rs:
